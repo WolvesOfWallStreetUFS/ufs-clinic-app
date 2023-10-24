46cde06affc3505c9d232851c0430ab0e13f4b87
Proffesor <pnguluvhe37@gmail.com>
2023-10-24 17:57:52 +0200

    updated main activity, color, MainActivity

--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/MainActivity.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/MainActivity.java
@@ -2,13 +2,44 @@ package wolvesofwallstreet.UFS.ufsclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button btnLogInMain, btnStuffLogin;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogInMain = findViewById(R.id.btnLogInMain);
+        btnStuffLogin = findViewById(R.id.btnStuffLogin);
+
+        btnLogInMain.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnStuffLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
