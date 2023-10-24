9d24cadb9c303f6f8495fbbaa032e45579719e78
vuyom <vuyombamba13@gmail.com>
2023-10-24 18:31:27 +0200

    updates: loginActivity, String value, color values

--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/LoginPage.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/LoginPage.java
@@ -3,12 +3,19 @@ package wolvesofwallstreet.UFS.ufsclinic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
 
+    private Button btnLogin;
+    private TextView txtEmail, txtPassword, txtForgotPassword, txtContinue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
     }
 }
