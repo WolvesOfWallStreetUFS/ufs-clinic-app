7d8d74f6262b1041d14d74cf2065858ebb02ce6f
Feizel <feizel.maduna@gmail.com>
2023-10-28 05:13:22 +0200

    Fixed a few errors on Onboarding xml

--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Onboarding.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Onboarding.java
@@ -2,13 +2,23 @@ package wolvesofwallstreet.UFS.ufsclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class Onboarding extends AppCompatActivity {
     private ImageSwitcher imgSwitcher;
     private Button btnSkipTour, btnNext;
+    private int[] onboardingImages = {
+            R.drawable.onb1,
+            R.drawable.onb2,
+            R.drawable.onb3
+    };
+    private int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +28,31 @@ public class Onboarding extends AppCompatActivity {
         imgSwitcher = findViewById(R.id.imgSwitcherONB);
         btnSkipTour = findViewById(R.id.btnSkipTour);
         btnNext = findViewById(R.id.btnNext);
+
+        imgSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imgView = new ImageView(Onboarding.this);
+                imgView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                return imgView;
+            }
+        });
+
+        btnSkipTour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Onboarding.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex = (currentIndex + 1) % onboardingImages.length;
+                imgSwitcher.setImageResource(onboardingImages[currentIndex]);
+            }
+        });
     }
 }
