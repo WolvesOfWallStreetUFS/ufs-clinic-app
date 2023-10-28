a65f59c2886a3a989bf8097e4c350c5ea9b841b9
Feizel <feizel.maduna@gmail.com>
2023-10-28 04:42:21 +0200

    Created the Onboarding design screen. Need to complete the java code.

--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Onboarding.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Onboarding.java
@@ -3,12 +3,19 @@ package wolvesofwallstreet.UFS.ufsclinic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
 
 public class Onboarding extends AppCompatActivity {
+    private ImageSwitcher imgSwitcher;
+    private Button btnSkipTour, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        imgSwitcher = findViewById(R.id.imgSwitcherONB);
+        btnSkipTour = findViewById(R.id.btnSkipTour);
     }
 }
