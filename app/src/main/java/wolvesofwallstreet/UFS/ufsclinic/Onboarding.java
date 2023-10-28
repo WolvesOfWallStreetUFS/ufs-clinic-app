dd67a8a9d776a23ada0a11817a7eb641ffe7394b
Feizel <feizel.maduna@gmail.com>
2023-10-28 05:59:35 +0200

    Worked on the logic for the Onboarding.java class. Updated the Onboarding xml as well.

--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Onboarding.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Onboarding.java
@@ -8,15 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
 public class Onboarding extends AppCompatActivity {
     private ImageSwitcher imgSwitcher;
+    private TextSwitcher txtSwitcher;
     private Button btnSkipTour, btnNext;
-    private int[] onboardingImages = {
+    private final int[] onboardingImages = {
             R.drawable.onb1,
             R.drawable.onb2,
             R.drawable.onb3
+    };
+    private final int[] onboardingLabels = {
+            R.string.ONB1_label,
+            R.string.ONB2_label,
+            R.string.ONB3_label
+    };
+    private final int[] onboardingDescriptions = {
+            R.string.ONB1_Desc,
+            R.string.ONB2_Desc,
+            R.string.ONB3_Desc
     };
     private int currentIndex = 0;
 
@@ -26,6 +39,7 @@ public class Onboarding extends AppCompatActivity {
         setContentView(R.layout.activity_onboarding);
 
         imgSwitcher = findViewById(R.id.imgSwitcherONB);
+        txtSwitcher = findViewById(R.id.txtSwitcherONB);
         btnSkipTour = findViewById(R.id.btnSkipTour);
         btnNext = findViewById(R.id.btnNext);
 
@@ -35,6 +49,17 @@ public class Onboarding extends AppCompatActivity {
                 ImageView imgView = new ImageView(Onboarding.this);
                 imgView.setScaleType(ImageView.ScaleType.FIT_CENTER);
                 return imgView;
+            }
+        });
+
+        txtSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                TextView txtView = new TextView(Onboarding.this);
+                txtView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                txtView.setTextColor(getResources().getColor(R.color.blue));
+                txtView.setTextSize(18);
+                return txtView;
             }
         });
 
@@ -52,6 +77,7 @@ public class Onboarding extends AppCompatActivity {
             public void onClick(View view) {
                 currentIndex = (currentIndex + 1) % onboardingImages.length;
                 imgSwitcher.setImageResource(onboardingImages[currentIndex]);
+                txtSwitcher.setText(getString(onboardingLabels[currentIndex]) + "\n" + getString(onboardingDescriptions[currentIndex]));
             }
         });
     }
