30379f2d91a907b68866dcb2da8b34d93c930d0f
vuyom <vuyombamba13@gmail.com>
2023-10-30 23:06:50 +0200

    working on home page

--- app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Home.java
+++ app/src/main/java/wolvesofwallstreet/UFS/ufsclinic/Home.java
@@ -3,8 +3,10 @@ package wolvesofwallstreet.UFS.ufsclinic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Home extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
